1137b778df595e36a413ca06826bbbf857c30c1c
Sardor Ubaydullayev <sardorubaydullayev20@gmail.com>
2023-06-26 02:53:33 +0500

    add json

--- src/main/java/us/fobros/fobros/controller/StateController.java
+++ src/main/java/us/fobros/fobros/controller/StateController.java
@@ -26,55 +26,64 @@ public class StateController {
 
     @PostMapping("/credit")
     public String paymentCredit() {
-        try {
-            String apiEndpoint = "https://back.usstartruckingllc.com/api/shipping/zip-codes/?search=";
-            String searchTerm = "";
-
-            // Construct the full URL with the search term
-            String url = apiEndpoint + searchTerm;
-
-            // Create an OkHttpClient instance
-            OkHttpClient client = new OkHttpClient();
-
-            // Create a Request
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-
-            // Send the request
-            Response response = client.newCall(request).execute();
-
-            // Get the response code and body
-            int responseCode = response.code();
-            String responseBody = response.body().string();
-
-            // Print the response
-            System.out.println("Response Code: " + responseCode);
-            System.out.println("Response Body: " + responseBody);
-
-// Parse the JSON response
-            JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
-
-// Extract the 'results' array from the JSON
-            JsonArray resultsArray = jsonObject.getAsJsonArray("results");
-
-// Create a Gson instance to convert JSON to Java objects
-            Gson gson = new Gson();
-
-// Convert the 'results' array to an array of Result objects
-            State[] states = gson.fromJson(resultsArray, State[].class);
-
-// Access and use the 'results' array
-            for (State state : states) {
-                stateService.saveState(state);
-            }            // Close the response
-            response.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 1; i < 3506; i++) {
+            save(i);
         }
         return null;
     }
+private String save(Integer i ){
+    try {
+        String apiEndpoint = "https://back.usstartruckingllc.com/api/shipping/zip-codes/";
+        String searchTerm2 = i.toString();
+// Replace with your desired search term
 
+        // Construct the full URL with the search term
+        String url = apiEndpoint + "?page=" + searchTerm2;
+
+        // Construct the full URL with the search term
+
+        // Create an OkHttpClient instance
+        OkHttpClient client = new OkHttpClient();
+
+        // Create a Request
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+
+        // Send the request
+        Response response = client.newCall(request).execute();
+
+        // Get the response code and body
+        int responseCode = response.code();
+        String responseBody = response.body().string();
+
+        // Print the response
+        System.out.println("Response Code: " + responseCode);
+        System.out.println("Response Body: " + responseBody);
+
+// Parse the JSON response
+        JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
+
+// Extract the 'results' array from the JSON
+        JsonArray resultsArray = jsonObject.getAsJsonArray("results");
+
+// Create a Gson instance to convert JSON to Java objects
+        Gson gson = new Gson();
+
+// Convert the 'results' array to an array of Result objects
+        State[] states = gson.fromJson(resultsArray, State[].class);
+
+// Access and use the 'results' array
+        for (State state : states) {
+            stateService.saveState(state);
+        }            // Close the response
+        response.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return null;
+}
 
 }
 
